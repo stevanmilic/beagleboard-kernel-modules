921d234493711660207c8a8583668ac7f3d77e6b
stevan <stevan.milic@yahoo.com>
2017-04-15 01:14:44 +0200

    Driver for servo motor is working(so,so...)

--- servo_motor_driver/pwm_servo.c
+++ servo_motor_driver/pwm_servo.c
@@ -2,22 +2,22 @@
 #include <linux/module.h> // Core header for loading LKMs into the kernel
 #include <linux/device.h> // Header to support the kernel Driver Model
 #include <linux/kernel.h> // Contains types, macros, functions for the kernel
-#include <linux/gpio.h> // Required for the GPIO functions
+#include <linux/pwm.h> // Required for the PWM functions
 #include <linux/fs.h> // Header for the Linux file system support
 #include <linux/mutex.h> // Required for the mutex functionality
 #include <asm/uaccess.h> // Required for the copy to user function
 
-#define  DEVICE_NAME "ebbled" // The device will appear at /dev/ebbled using this value
+#define  DEVICE_NAME "ebbservo" // The device will appear at /dev/ebbservo using this value
 #define  CLASS_NAME  "ebb" // The device class -- this is a character device driver
 
 MODULE_LICENSE("GPL"); // The license type -- this affects available functionality
 MODULE_AUTHOR("Stevan Milic"); // The author -- visible when you use modinfo
-MODULE_DESCRIPTION("A LED test driver for the BBB"); // The description -- see modinfo
+MODULE_DESCRIPTION("A Servo Motor driver for the BBB"); // The description -- see modinfo
 MODULE_VERSION("0.1"); // A version number to inform users
 
 static int    majorNumber;          // Stores the device number -- determined automatically
-static struct class*  ebbledClass  = NULL; // The device-driver class struct pointer
-static struct device* ebbledDevice = NULL; // The device-driver device struct pointer
+static struct class*  ebbservoClass  = NULL; // The device-driver class struct pointer
+static struct device* ebbservoDevice = NULL; // The device-driver device struct pointer
 static char   message[256] = {0};          // Buffer for sending and receiving messages from user space
 static bool     devRead = 0;               // Flag for defining if reading is done
 
@@ -38,12 +38,14 @@ static struct file_operations fops = {
 	.release = dev_release,
 };
 
-static unsigned int gpioLED = 23;  // hard coding the LED gpio for this example to GPIO23
-static bool ledOn = 0; // Is the LED on or off? Used to invert its state (off by default)
+static unsigned int pwmServoId = 4;  // hard coding the Servo pwm for this example to PWM1A
+static struct pwm_device *pwmServoDevice = NULL;
+static int dutyCycleServo = 1458333; // 0.5ms pulse -> position "0"
+static int periodServo = 16666667; //16.66ms period -> 60hz
 
-static DEFINE_MUTEX(ebbled_mutex); // A macro that is used to declare a new mutex that is visible in this file
-                                    // results in a semaphore variable ebbled_mutex with value 1 (unlocked)
-                                    // DEFINE_MUTEX_LOCKED() results in a variable with value 0 (locked)
+static DEFINE_MUTEX(ebbservo_mutex); // A macro that is used to declare a new mutex that is visible in this file
+// results in a semaphore variable ebbservo_mutex with value 1 (unlocked)
+// DEFINE_MUTEX_LOCKED() results in a variable with value 0 (locked)
 
 /** @brief The LKM initialization function
  *  The static keyword restricts the visibility of the function to within this C file. The __init
@@ -51,49 +53,52 @@ static DEFINE_MUTEX(ebbled_mutex); // A macro that is used to declare a new mute
  *  time and that it can be discarded and its memory freed up after that point.
  *  @return returns 0 if successful
  */
-static int __init ledgpio_init(void)
+static int __init servogpio_init(void)
 {
-	printk(KERN_INFO "GPIO_TEST: Initializing the GPIO_TEST LKM\n");
+	printk(KERN_INFO "GPIO_TEST: Initializing the PWM_TEST LKM\n");
 
-	mutex_init(&ebbled_mutex); // Initialize the mutex lock dynamically at runtime
-
-	if (!gpio_is_valid(gpioLED)) {
-		printk(KERN_INFO "GPIO_TEST: invalid LED GPIO\n");
-		return -ENODEV;
-	}
+	mutex_init(&ebbservo_mutex); // Initialize the mutex lock dynamically at runtime
 
 	majorNumber = register_chrdev(0, DEVICE_NAME, &fops);
 	if (majorNumber < 0) {
-		printk(KERN_ALERT "EBBled failed to register a major number\n");
+		printk(KERN_ALERT "Ebbservo failed to register a major number\n");
 		return majorNumber;
 	}
-	printk(KERN_INFO "EBBled: registered correctly with major number %d\n", majorNumber);
+	printk(KERN_INFO "Ebbservo: registered correctly with major number %d\n", majorNumber);
 
 	// Register the device class
-	ebbledClass = class_create(THIS_MODULE, CLASS_NAME);
-	if (IS_ERR(ebbledClass)) {               // Check for error and clean up if there is
+	ebbservoClass = class_create(THIS_MODULE, CLASS_NAME);
+	if (IS_ERR(ebbservoClass)) {               // Check for error and clean up if there is
 		unregister_chrdev(majorNumber, DEVICE_NAME);
 		printk(KERN_ALERT "Failed to register device class\n");
-		return PTR_ERR(ebbledClass);          // Correct way to return an error on a pointer
+		return PTR_ERR(ebbservoClass);          // Correct way to return an error on a pointer
 	}
-	printk(KERN_INFO "EBBled: device class registered correctly\n");
+	printk(KERN_INFO "Ebbservo: device class registered correctly\n");
 
 	// Register the device driver
-	ebbledDevice = device_create(ebbledClass, NULL, MKDEV(majorNumber, 0), NULL, DEVICE_NAME);
-	if (IS_ERR(ebbledDevice)) {              // Clean up if there is an error
-		class_destroy(ebbledClass);           // Repeated code but the alternative is goto statements
+	ebbservoDevice = device_create(ebbservoClass, NULL, MKDEV(majorNumber, 0), NULL, DEVICE_NAME);
+	if (IS_ERR(ebbservoDevice)) {              // Clean up if there is an error
+		class_destroy(ebbservoClass);           // Repeated code but the alternative is goto statements
 		unregister_chrdev(majorNumber, DEVICE_NAME);
 		printk(KERN_ALERT "Failed to create the device\n");
-		return PTR_ERR(ebbledDevice);
+		return PTR_ERR(ebbservoDevice);
 	}
 
-	printk(KERN_INFO "EBBled: device class created correctly\n"); // Made it! device was initialized
+	printk(KERN_INFO "Ebbservo: %d device class created correctly\n", pwmServoId); // Made it! device was initialized
 
-	ledOn = 1;
-	gpio_request(gpioLED, "sysfs"); // gpioLED is hardcoded to 23, request it
-	gpio_direction_output(gpioLED, ledOn); // Set the gpio to be in output mode and on
-	// gpio_set_value(gpioLED, ledOn); // Not required as set by line above (here for reference)
-	gpio_export(gpioLED, false);
+	pwmServoDevice = pwm_request(pwmServoId, "sysfs"); //pwm_id is hardcoded to 2, request it
+	if (IS_ERR(pwmServoDevice)) {
+		printk(KERN_ALERT "Ebbservo failed to get pwm servo device\n");
+		return PTR_ERR(pwmServoDevice);
+	}
+
+	pwm_config(pwmServoDevice, dutyCycleServo, periodServo);
+
+	printk(KERN_INFO "Ebbservo: Period: %d Duty Cycle: %d\n", pwm_get_period(pwmServoDevice), pwm_get_duty_cycle(pwmServoDevice));
+
+	pwm_enable(pwmServoDevice);
+
+	printk(KERN_INFO "Ebbservo: Servo enabled successfully\n");
 
 	return 0;
 }
@@ -102,16 +107,15 @@ static int __init ledgpio_init(void)
  *  Similar to the initialization function, it is static. The __exit macro notifies that if this
  *  code is used for a built-in driver (not a LKM) that this function is not required.
  */
-static void __exit ledgpio_exit(void)
+static void __exit servogpio_exit(void)
 {
-	gpio_set_value(gpioLED, 0);
-	gpio_unexport(gpioLED);
-	gpio_free(gpioLED);
-	device_destroy(ebbledClass, MKDEV(majorNumber, 0)); // remove the device
-	class_unregister(ebbledClass); // unregister the device class
-	class_destroy(ebbledClass); // remove the device class
+	pwm_disable(pwmServoDevice);
+	pwm_free(pwmServoDevice);
+	device_destroy(ebbservoClass, MKDEV(majorNumber, 0)); // remove the device
+	class_unregister(ebbservoClass); // unregister the device class
+	class_destroy(ebbservoClass); // remove the device class
 	unregister_chrdev(majorNumber, DEVICE_NAME); // unregister the major number
-	mutex_destroy(&ebbled_mutex); // destroy the dynamically-allocated mutex
+	mutex_destroy(&ebbservo_mutex); // destroy the dynamically-allocated mutex
 	printk(KERN_INFO "GPIO_TEST: Goodbye from the LKM!\n");
 }
 
@@ -122,13 +126,13 @@ static void __exit ledgpio_exit(void)
 */
 static int dev_open(struct inode *inodep, struct file *filep)
 {
-	if (!mutex_trylock(&ebbled_mutex)) {
+	if (!mutex_trylock(&ebbservo_mutex)) {
 		// Try to acquire the mutex (i.e., put the lock on/down)
 		// returns 1 if successful and 0 if there is contention
-		printk(KERN_ALERT "EBBled: Device in use by another process");
+		printk(KERN_ALERT "ebbservo: Device in use by another process");
 		return -EBUSY;
 	}
-	printk(KERN_INFO "EBBled: Device has been opened\n");
+	printk(KERN_INFO "ebbservo: Device has been opened\n");
 	return 0;
 }
 
@@ -148,17 +152,17 @@ static ssize_t dev_read(struct file *filep, char *buffer, size_t len, loff_t *of
 		return (devRead = 0);
 	}
 
-	sprintf(message, "%d", ledOn);
+	sprintf(message, "%d", pwm_get_duty_cycle(pwmServoDevice));
 	size_of_message = strlen(message) + 1;
 
 	error_count = copy_to_user(buffer, message, size_of_message);
 
 	if (error_count == 0) {         // if true then have success
-		printk(KERN_INFO "EBBled: Sent %d character to the user\n", size_of_message);
+		printk(KERN_INFO "ebbservo: Sent %d character to the user\n", size_of_message);
 		devRead = 1;
 		return size_of_message;
 	} else {
-		printk(KERN_INFO "EBBled: Failed to send %d characters to the user\n", error_count);
+		printk(KERN_INFO "ebbservo: Failed to send %d characters to the user\n", error_count);
 		return -EFAULT;              // Failed -- return a bad address message (i.e. -14)
 	}
 }
@@ -176,11 +180,11 @@ static ssize_t dev_write(struct file *filep, const char *buffer, size_t len, lof
 	int error_count = copy_from_user(message, buffer, len);
 
 	if (error_count == 0) {
-		printk(KERN_INFO "EBBled: Received %zu characters from the user\n", len);
-		sscanf(message, "%d", &ledOn);
-		gpio_set_value(gpioLED, ledOn);
+		printk(KERN_INFO "ebbservo: Received %zu characters from the user\n", len);
+		sscanf(message, "%d", &dutyCycleServo);
+		pwm_config(pwmServoDevice, dutyCycleServo, periodServo);
 	} else {
-		printk(KERN_INFO "EBBled: Failed to receive %d characters from the user\n", error_count);
+		printk(KERN_INFO "ebbservo: Failed to receive %d characters from the user\n", error_count);
 		return -EFAULT;              // Failed -- return a bad address message (i.e. -14)
 	}
 
@@ -194,8 +198,8 @@ static ssize_t dev_write(struct file *filep, const char *buffer, size_t len, lof
  */
 static int dev_release(struct inode *inodep, struct file *filep)
 {
-	mutex_unlock(&ebbled_mutex); // Releases the mutex (i.e., the lock goes up)
-	printk(KERN_INFO "EBBled: Device successfully closed\n");
+	mutex_unlock(&ebbservo_mutex); // Releases the mutex (i.e., the lock goes up)
+	printk(KERN_INFO "ebbservo: Device successfully closed\n");
 	return 0;
 }
 
@@ -203,5 +207,5 @@ static int dev_release(struct inode *inodep, struct file *filep)
  *  identify the initialization function at insertion time and the cleanup function (as
  *  listed above)
  */
-module_init(ledgpio_init);
-module_exit(ledgpio_exit);
+module_init(servogpio_init);
+module_exit(servogpio_exit);
