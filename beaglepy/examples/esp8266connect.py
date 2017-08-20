09c6bd89c4db8c09cc606f4cfa7710c2907b111b
Stevan Milic <stevan.milic@yahoo.com>
2017-08-20 20:47:40 +0200

    Added support for BeagleBone java client

--- beaglepy/examples/esp8266connect.py
+++ beaglepy/examples/esp8266connect.py
@@ -1,6 +1,15 @@
-"""Module for using esp8266 wifi module with uart pins on the bbb"""
+"""
+Module for using esp8266 wifi module with uart pins on the bbb
+
+Args:
+    param1 (str): esp8266 uart pin
+    param2 (str): ssid to connect to
+    param3 (str): password for given ssid
+    param4 (str): esp8266 port listener
+"""
 
 from time import sleep
+from re import search
 import signal
 import sys
 from ..wrappers.uart import Uart
@@ -12,7 +21,7 @@ EXTERNAL_LED_PIN = 'P8_19'
 INTERRUPT_PIN = 'P9_12'
 
 BAUDRATE = 115200
-PORT = 80
+PORT = 5000
 
 
 def enum(**enums):
@@ -20,24 +29,29 @@ def enum(**enums):
     return type('Enum', (), enums)
 
 
-Status = enum(ERR=['ERROR', 'Fail'], OK=[
-    'OK', 'ready', 'no change', 'SEND OK'])
+Status = enum(ERR=['ERROR', 'Fail'],
+              OK=['OK', 'ready', 'no change', 'SEND OK', 'CONNECT'])
 
 
 def main():
     """Main program where we are defining and configuring
-    wifi module and external led diod. Wifi module will be
-    listening on specific port and upon receiving request
-    it will change led diod state"""
+    wifi module, external led diod and interrupt gpio.
+    Wifi module will be listening on specific port and
+    upon receiving request it will change led diod state
+    """
 
-    uart = Uart(UART_PIN, BAUDRATE)
+    uart_pin = sys.argv[1] if len(sys.argv) >= 2 else UART_PIN
+
+    uart = Uart(uart_pin, BAUDRATE)
     setup_wifi(uart)
 
-    external_led_gpio = Gpio(EXTERNAL_LED_PIN, OUTPUT)
+    # external_led_gpio = Gpio(EXTERNAL_LED_PIN, OUTPUT)
+    external_led_gpio = None
 
-    interrupt_gpio = Interrupt(INTERRUPT_PIN)
+    # interrupt_gpio = Interrupt(INTERRUPT_PIN)
+    interrupt_gpio = None
 
-    def signal_handler(signal, frame):
+    def signal_handler(signum, frame):
         """Handler for ctrl+c interrupt - program ends"""
         external_led_gpio.free()
         uart.free()
@@ -47,7 +61,8 @@ def main():
     signal.signal(signal.SIGINT, signal_handler)
 
     while True:
-        process_request(uart, interrupt_gpio, external_led_gpio)
+        (external_led_gpio, interrupt_gpio) = process_request(
+            uart, interrupt_gpio, external_led_gpio)
         sleep(0.3)
 
 
@@ -59,17 +74,20 @@ def setup_wifi(uart):
     send_cmd(uart, 'AT+CWMODE=1')  # Client mode
     send_cmd(uart, 'AT+RST')  # Reset the module
 
-    send_cmd(uart, 'AT+CWLAP', 10)  # List available AP
+    # send_cmd(uart, 'AT+CWLAP', 10)  # List available AP
 
-    ssid = raw_input('Enter ssid of network you want to connect:')
-    password = raw_input('Enter password for specific network:')
+    ssid = sys.argv[2] if len(sys.argv) >= 3 else raw_input(
+        'Enter ssid of network you want to connect:')
+    password = sys.argv[3] if len(sys.argv) >= 4 else raw_input(
+        'Enter password for specific network:')
     con_command = 'AT+CWJAP=\"' + ssid + '\",\"' + password + '\"'
 
     send_cmd(uart, con_command, 5)  # Connect to SSID with supplied password
     send_cmd(uart, 'AT+CIFSR', 5)  # Get local IP address
 
     send_cmd(uart, 'AT+CIPMUX=1')  # Enable multiplex mode
-    send_cmd(uart, 'AT+CIPSERVER=1,' + str(PORT))  # Server mode
+    port = sys.argv[4] if len(sys.argv) >= 5 else str(PORT)
+    send_cmd(uart, 'AT+CIPSERVER=1,' + port)  # Server mode
 
 
 def send_cmd(uart, command, wait_time=1, retry=5):
@@ -102,7 +120,6 @@ def process_request(uart, interrupt_gpio, led_gpio):
     """Function for processing request from client"""
 
     has_request = False
-    client_id = '0'
 
     while uart.is_busy():
         request = uart.read()
@@ -110,12 +127,20 @@ def process_request(uart, interrupt_gpio, led_gpio):
         ipd_str = '+IPD,'
         if ipd_str in request:
             has_request = True
-            client_id = request[request.find(ipd_str) + len(ipd_str)]
+            data = search(':(.*)0,CLOSED', request).group(1)
+            (pin_type, pin_id, pin_value) = [
+                t(s) for t, s in zip((str, str, int), data.split())]
 
-    if has_request and not interrupt_gpio.attached:
-        print 'Interrupt attached...'
-        interrupt_gpio.attach_interrupt(
-            external_led_handler, uart, led_gpio, client_id)
+    if has_request:
+        if pin_type == 'l':
+            led_gpio = Gpio(pin_id, OUTPUT) if led_gpio is None else led_gpio
+            led_gpio.write(pin_value)
+        elif pin_type == 'i' and led_gpio is not None:
+            interrupt_gpio = Interrupt(pin_id)
+            interrupt_gpio.attach_interrupt(external_led_handler, led_gpio)
+            print 'Interrupt attached on pin ' + pin_id
+
+    return (led_gpio, interrupt_gpio)
 
 
 def send_message(uart, message, client_id='0'):
@@ -146,17 +171,12 @@ def send_message(uart, message, client_id='0'):
 def external_led_handler(*args):
     """Method which is triggered when event is detected on interrupt gpio pin
     """
-    uart = args[0]
-    led_gpio = args[1]
-    client_id = args[2]
+    led_gpio = args[0]
 
     state = HIGH if led_gpio.state is LOW else LOW
     led_gpio.write(state)
 
-    message = "HTTP/1.1 200 OK\n" + "Content-Type: text/html\n" + \
-        "\n" + '<html><body>You\'ve changed led\'s state, now it\'s ' + \
-        str(state) + '</body></html>\n'
-    send_message(uart, message, client_id)
+    print 'Interrupt detected... Led value is ' + str(state) + ' now'
 
 
 if __name__ == "__main__":
